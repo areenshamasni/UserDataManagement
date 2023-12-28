89a41a0004c46ccb0317d8c7fb8faf89cc44f667
samakhraim <samakhraim12@gmail.com>
2023-12-29 01:27:35 +0200

    inserting data to mondo db error solved

--- src/edu/najah/cap/data/mongodb/DataInserter.java
+++ src/edu/najah/cap/data/mongodb/DataInserter.java
@@ -5,18 +5,23 @@ import edu.najah.cap.activity.UserActivity;
 import edu.najah.cap.exceptions.BadRequestException;
 import edu.najah.cap.exceptions.NotFoundException;
 import edu.najah.cap.exceptions.SystemBusyException;
+import edu.najah.cap.exceptions.Util;
 import edu.najah.cap.iam.IUserService;
 import edu.najah.cap.iam.UserProfile;
+import edu.najah.cap.iam.UserType;
 import edu.najah.cap.payment.IPayment;
 import edu.najah.cap.payment.Transaction;
 import edu.najah.cap.posts.IPostService;
 import edu.najah.cap.posts.Post;
 import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class DataInserter {
     private final MongoDataInserter mongoDataInserter;
+    private static final Logger logger = LoggerFactory.getLogger(DataInserter.class);
 
     public DataInserter(MongoDataInserter mongoDataInserter) {
         this.mongoDataInserter = mongoDataInserter;
@@ -24,55 +29,39 @@ public class DataInserter {
 
     public void insertData(IUserActivityService userActivityService, IPayment paymentService,
                            IUserService userService, IPostService postService) {
-        String userId = null;
-        try {
-            for (int i = 0; i < 100; i++) {
-                userId = "user" + i;
-                Util.setSkipValidation(true);
-                List<UserActivity> userActivities = userActivityService.getUserActivity(userId);
-                if (userActivities != null){
+        Util.setSkipValidation(true);
+        for (int i = 0; i < 100; i++) {
+            String userId = "user" + i;
+            try {
+                UserProfile userProfile = userService.getUser(userId);
+                UserType userType = userProfile.getUserType();
+                if (!UserType.NEW_USER.equals(userType)) {
+                    List<UserActivity> userActivities = userActivityService.getUserActivity(userId);
                     for (UserActivity userActivity : userActivities) {
-                        try {
-                            mongoDataInserter.insertDocument("activities", userActivity, this::mapUserActivityToDocument);
-                        } catch (Exception e) {
-                            logger.error("Error inserting user activity document for userId: " + userId, e);
-                        }
-                    }}
-
-                List<Transaction> transactions = paymentService.getTransactions(userId);
-                if (transactions != null) {
-                    for (Transaction transaction : transactions) {
-                        try {
+                        mongoDataInserter.insertDocument("activities", userActivity, this::mapUserActivityToDocument);
+                    }
+                }
+                if (UserType.PREMIUM_USER.equals(userType)) {
+                    List<Transaction> transactions = paymentService.getTransactions(userId);
+                    if (transactions != null) {
+                        for (Transaction transaction : transactions) {
                             mongoDataInserter.insertDocument("payments", transaction, this::mapTransactionToDocument);
-                        } catch (Exception e) {
-                            logger.error("Error inserting payment document for userId: " + userId, e);
                         }
                     }
                 }
-
-                UserProfile userProfile = userService.getUser(userId);
-                if (userProfile != null) {
-                    try {
-                        mongoDataInserter.insertDocument("users", userProfile, this::mapUserProfileToDocument);
-                    } catch (Exception e) {
-                        logger.error("Error inserting user profile document for userId: " + userId, e);
-                    }}
+                mongoDataInserter.insertDocument("users", userProfile, this::mapUserProfileToDocument);
 
                 List<Post> posts = postService.getPosts(userId);
-                if (posts != null) {
-                    for (Post post : posts) {
-                        try {
-                            mongoDataInserter.insertDocument("posts", post, this::mapPostToDocument);
-                        } catch (Exception e) {
-                            logger.error("Error inserting post document for userId: " + userId, e);
-                        }
-                    }}
+                for (Post post : posts) {
+                    mongoDataInserter.insertDocument("posts", post, this::mapPostToDocument);
+                }
+            } catch (Exception e) {
+                logger.error("Error in insertData method", e);
             }
-        } catch (SystemBusyException | BadRequestException | NotFoundException e) {
-            logger.error("Error in insertData method", e);
-        } finally {
-            Util.setSkipValidation(false);
         }
+        Util.setSkipValidation(false);
+
+
     }
 
     private Document mapUserActivityToDocument(UserActivity userActivity) {
